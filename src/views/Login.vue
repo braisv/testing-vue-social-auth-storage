481ea3c0b95fef102e55f60cf588d343ca176567
braisv <brais.vidal.rodriguez@gmail.com>
2020-04-01 18:17:35 +0200

    login view

---
+++ src/views/Login.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-layout justify-center mt-4>
+    <v-flex xs12 sm8 md6 xl4>
+      <v-card>
+        <v-card-text
+          class="display-1 text-uppercase white--text text-center"
+          :class="noAccount ? 'success' : 'accent'"
+        >
+          <span v-if="!noAccount">Log In</span>
+          <span v-if="noAccount">Sig Up</span>
+        </v-card-text>
+
+        <v-card-text>
+          <v-btn block color="error">
+            <v-icon left dark>fab fa-google</v-icon>
+            Google
+          </v-btn>
+          <v-btn block color="info" class="mt-2">
+            <v-icon left dark>fab fa-facebook-f</v-icon>
+            Facebook
+          </v-btn>
+        </v-card-text>
+
+        <v-card-text>
+          <v-btn block @click="noAccount = !noAccount">{{ noAccount ? 'Do yu already have account? Log in here' : 'No account? Sign up here' }}</v-btn>
+        </v-card-text>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data() {
+    return {
+      noAccount: false
+    };
+  }
+};
+</script>
