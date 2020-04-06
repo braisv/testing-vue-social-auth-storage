9470714353765de04de757113323381f0bc16619
braisv <brais.vidal.rodriguez@gmail.com>
2020-04-06 13:08:26 +0200

    print image

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -15,6 +15,10 @@
               <v-btn class="mr-5" color="primary" @click="$refs.button.click()">Search Image</v-btn>
               <v-btn color="secondary" :disabled="!file">Upload Image</v-btn>
           </v-card-text>
+          <v-card-text v-if="file">
+              <h4>{{ file.name }}</h4>
+              <v-img :src="urlTemp"></v-img>
+          </v-card-text>
       </v-card>
     </v-col>
   </v-row>
@@ -27,13 +31,21 @@ export default {
   name: "Settings",
   data() {
       return {
-          file: null
+          file: null,
+          urlTemp: ''
       }
   },
   methods: {
       searchImage(e) {
           console.log(e.target.files[0])
           this.file = e.target.files[0]
+
+          const reader = new FileReader();
+          reader.readAsDataURL(this.file);
+          reader.onload = (e) => {
+              this.urlTemp = e.target.result
+              // console.log(urlTemp)
+          }
       }
   },
     computed: {
